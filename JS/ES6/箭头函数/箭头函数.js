431fa078085088dd4b740719aad5f544465ba42f
王一丁 <554255380@qq.com>
2019-12-25 09:28:55 +0800

    12/25 JS Vue 更新

--- JS/ES6/箭头函数/箭头函数.js
+++ JS/ES6/箭头函数/箭头函数.js
@@ -1,1 +1,28 @@
+/*
+ * @Description: 
+ * @Autor: Leon
+ * @Date: 2019-11-29 20:33:56
+ * @LastEditors: Leon
+ * @LastEditTime: 2019-12-24 09:39:23
+ */
 // TODO:还没总结
+
+// ES6 箭头函数
+// 几种写法
+// 1. 
+let add = a => b => c => a + b + c
+add(1)(2)(3) // 6
+
+// 2.
+let add = _=>{}
+
+// 3. 
+// return a
+let add = (a=1)=>{
+    return a // 1
+}
+// 简写
+let add = a=>a;
+// return 一个对象
+let add = _=>({a:1,b:2})
+
---
+++ 框架/React和Vue.md
@@ -0,0 +1,13 @@
+<!--
+ * @Description: 
+ * @Autor: Leon
+ * @Date: 2019-12-24 09:44:54
+ * @LastEditors: Leon
+ * @LastEditTime: 2019-12-24 09:48:06
+ -->
+1. 语法不同
+   1. vue是template语法
+   2. React是JSX语法
+2. 设计模式不同[声明式,命令式](https://juejin.im/post/5950b0c56fb9a06bca0b7c04)
+   1. React是声明范式
+   2. Vue主要是命令式
